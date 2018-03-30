14d40a64d413c17d127b48f7b0c0a85155273999
Shun Yao <ysmiracle@hotmail.com>
2018-03-30 06:33:04 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,73 @@
+#require "pry"
+#require 'json'
+#data = JSON.parse(File.read('spec/fixtures/contestants.json'))
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = ""
+  
+  data.each do |season_num, contestants|
+    if season_num == season
+      contestants.each do |contestant|
+        if contestant["status"] == "Winner"
+          winner = contestant["name"].split(" ").first
+        end
+      end
+    end
+  end
+  
+  winner
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  name = ""
+  
+  data.each do |season_num, contestants|
+      contestants.each do |contestant|
+        if contestant["occupation"] == occupation
+          name = contestant["name"]
+        end
+      end
+  end
+  
+  name
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  
+  data.each do |season_num, contestants|
+    contestants.each do |contestant|
+      if contestant["hometown"] == hometown
+          count += 1
+      end
+    end
+  end
+  
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season_num, contestants|
+    contestants.each do |contestant|
+      if contestant["hometown"] == hometown
+        return contestant["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  sum = 0
+  count = 0
+  
+  data.each do |season_num, contestants|
+    if season_num == season
+      contestants.each do |contestant|
+        sum += contestant["age"].to_f
+        count += 1
+      end
+    end
+  end
+  
+  (sum/count).round
 end
